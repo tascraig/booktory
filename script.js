2a7e7c2f7c14a0f3aca598e560a3e09d562abf6c
tascraig <tcraig@pursuit.org>
2024-03-22 15:30:58 -0400

    CSS Layout updated for booklist section

---
+++ script.js
@@ -0,0 +1,7 @@
+let title = document.getElementById('book-title');
+let author = document.getElementById('book-author');
+let image = document.getElementById('book-url');
+let price = document.getElementById('book-price');
+let stock = document.getElementById('stock-drop');
+
+.`addEventListener('submit')`
